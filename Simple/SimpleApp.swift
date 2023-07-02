a7385e1cd8f05720c32c0c3d6b776dcac49bed19
Philipp <pd95@users.noreply.github.com>
2023-07-02 22:18:07 +0200

    Initial Commit

---
+++ Simple/SimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleApp.swift
+//  Simple
+//
+//  Created by Philipp on 02.07.23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
